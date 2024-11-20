7e59174b348117a20d74163cb49e1b4526c04db3
Ravi Ranjan  Ojha <raviranjanojha999@gmail.com>
2024-11-20 14:15:55 +0530

    Initial Commit

---
+++ swiftui-weather/swiftui_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_weatherApp.swift
+//  swiftui-weather
+//
+//  Created by Ravi Ranjan  Ojha on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
